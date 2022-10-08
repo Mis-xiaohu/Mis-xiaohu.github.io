0983a27fa62e3708b7fe2e9514093ab036914055
小虎 <453650969@qq.com>
2022-10-08 20:15:00 +0800

    Site updated: 2022-10-08 20:15:00

---
+++ js/weather.js
@@ -0,0 +1,23 @@
+WIDGET = {
+    "CONFIG": {
+        "modules": "01234",
+        "background": "5",
+        "tmpColor": "FF9900",
+        "tmpSize": "16",
+        "cityColor": "4A86E8",
+        "citySize": "16",
+        "aqiColor": "FF9900",
+        "aqiSize": "16",
+        "weatherIconSize": "24",
+        "alertIconSize": "18",
+        "padding": "10px 10px 10px 10px",
+        "shadow": "0",
+        "language": "auto",
+        "fixed": "true",
+        "vertical": "top",
+        "horizontal": "left",
+        "left": "128",
+        "top": "12",
+        "key": "2f015b843d92462c8397086ab49e1d8f"
+    }
+}
